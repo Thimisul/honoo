c2f456fda69de3079c3b2a70a18f3a0423dca747
Thimisul <thimisull@gmail.com>
2019-10-28 22:46:13 +0000

    Adicionado pesquisa por usuarios no models event

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,10 +3,10 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :participants
 
-  Reducer = Rack::Reducer.new(
+  Reducer = Rack::Reducer.new( #pesquisa com todos ou alguns parametros
     self.all,
     ->(rdate:) { by_date(rdate) },
-    ->(start_date:) {where("startDAte >= ?", start_date)},
+    ->(start_date: Time.now) {where("startDAte >= ?", start_date)}, #time.now toda pesquisa comeca com a hora de inicio da requisicao
     ->(end_date:) {where("endDate <= ?", end_date)},
     ->(user:) { where(user_id: user) },
     ->(event_type:) {where(event_type_id: event_type)}
